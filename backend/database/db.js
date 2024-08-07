33e5e8addda2d1f71bb4b3466ec9caa8e31ad0f0
anusha-0129 <anusha.adhina29@gmail.com>
2024-08-07 16:44:54 +0530

    changed data

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,7 +5,7 @@ const username=process.env.MONGO_DB_USERNAME;
 const password=process.env.MONGO_DB_PASSWORD;
 const url=process.env.MONGO_DB_URL;
 const database=()=>{
-mongoose.connect(`mongodb+srv://${username}:${password}@${url}/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>console.log("mongodb connected")).catch((err)=>
+mongoose.connect(`mongodb+srv://${username}:${password}@${url}/test?retryWrites=true&w=majority`).then(()=>console.log("mongodb connected")).catch((err)=>
 console.error(err));
 };
 export default database;
