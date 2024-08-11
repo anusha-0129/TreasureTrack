15d686147e7255e76f69f01274f15ae98e0ccc95
anusha-0129 <anusha.adhina29@gmail.com>
2024-08-11 16:06:19 +0530

    added code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import database from "./database/db.js";
 import { authRouter } from './routes/auth.js';
@@ -24,7 +25,11 @@ app.use('/auth', authRouter);
 app.use('/incomes', incomeRouter);
 app.use('/expenses', expenseRouter);
 app.use(voiceCommandRouter);
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log("Server is Running");
 });
