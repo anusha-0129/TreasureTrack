75c394cf94f61c0917a8399c21eced0377a367c2
anusha-0129 <anusha.adhina29@gmail.com>
2024-08-11 16:22:24 +0530

    changed code

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import path from 'path';
-import url from 'url';
-
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -10,44 +7,24 @@ import { authRouter } from './routes/auth.js';
 import { incomeRouter } from './routes/income.js';
 import { expenseRouter } from './routes/expense.js';
 import voiceCommandRouter from './routes/voiceCommand.js';
-
-
 dotenv.config();
 
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://treasuretrack.onrender.com"],
+  origin: ["http://localhost:5173","https://treasuretrack.onrender.com"],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
 app.use(cookieParser());
-
-
 database();
-
-
 app.use('/auth', authRouter);
 app.use('/incomes', incomeRouter);
 app.use('/expenses', expenseRouter);
 app.use(voiceCommandRouter);
 
-// Serve static files from the Vite build output directory
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
-
-// The catch-all handler for serving the Vite app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
-});
-
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is Running");
 });
