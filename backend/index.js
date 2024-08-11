0cc3ed27129e7b2d0e6dac6214243dd7b2ee184b
anusha-0129 <anusha.adhina29@gmail.com>
2024-08-11 16:12:00 +0530

    modified code

--- backend/index.js
+++ backend/index.js
@@ -8,28 +8,37 @@ import { authRouter } from './routes/auth.js';
 import { incomeRouter } from './routes/income.js';
 import { expenseRouter } from './routes/expense.js';
 import voiceCommandRouter from './routes/voiceCommand.js';
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173","https://treasuretrack.onrender.com"],
+  origin: ["http://localhost:5173", "https://treasuretrack.onrender.com"],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
 app.use(cookieParser());
 database();
+
 app.use('/auth', authRouter);
 app.use('/incomes', incomeRouter);
 app.use('/expenses', expenseRouter);
 app.use(voiceCommandRouter);
+
+
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
+
 app.listen(PORT, () => {
-  console.log("Server is Running");
+  console.log(`Server is running on port ${PORT}`);
 });
